4621805f98763861a8474dd76a4c0ad1809062f9
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-05-30 15:26:40 -0600

    added 'window' class

---
+++ graphics.py
@@ -0,0 +1,27 @@
+from tkinter import TK, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title = "Maze Solver"
+        self.canvas = Canvas(self.root, bg="white" width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.running = True
+
+        while self.running:
+            self.redraw()
+
+        print("Window closed...")
+
+    def close(self):
+        self.running = False
+
