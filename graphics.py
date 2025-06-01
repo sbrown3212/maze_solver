e7bf085fcc348c554917d1969f5029720db1f53f
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-05-31 23:18:20 -0600

    added 'Line' and 'Point' classes. Added test lines to main()

--- graphics.py
+++ graphics.py
@@ -1,11 +1,11 @@
-from tkinter import TK, BOTH, Canvas
+from tkinter import Tk, BOTH, Canvas
 
 
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title = "Maze Solver"
-        self.__canvas = Canvas(self.root, bg="white" width=width, height=height)
+        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -17,11 +17,36 @@ class Window:
     def wait_for_close(self):
         self.__running = True
 
-        while self.running:
+        while self.__running:
             self.redraw()
 
         print("Window closed...")
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point1.x,
+            self.point1.y,
+            self.point2.x,
+            self.point2.y,
+            fill=fill_color,
+            width=2
+        )
 
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from graphic import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+
+    point_a = Point(100, 100)
+    point_b = Point(100, 200)
+    point_c = Point(200, 200)
+
+    line_1 = Line(point_a, point_b)
+    line_2 = Line(point_b, point_c)
+
+    win.draw_line(line_1, "red")
+    win.draw_line(line_2, "black")
+
     win.wait_for_close()
 
 
-if __name__ = '__main__':
+
+if __name__ == '__main__':
     main()
