dc9c3e1e6d8feb0929f5af74df6a41f6732f396d
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-06-02 14:50:00 -0600

    created 'maze' class

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
 from graphics import Window, Point, Line
 from cell import Cell
+from maze import Maze
 
 def main():
     win = Window(800, 600)
 
-    c1 = Cell(win)
-    c1.has_left_wall = False
-    c1.draw(500,300, 600, 400)
-
-    c2 = Cell(win)
-    c2.has_top_wall = False
-    c2.draw(100, 400, 200, 500)
-
-    c1.draw_move(c2, undo=False)
-
-    x1 = 100
-    y1 = 100
-    x2 = 200
-    y2 = 200
-
-    c3 = Cell(win)
-    c3.draw(x1, y1, x2, y2)
-
-    x1 = 250
-    y1 = 250
-    x2 = 350
-    y2 = 350
-
-    c4 = Cell(win).draw(x1, y1, x2, y2)
-
-    c3.draw_move(c1, undo=True)
+    Maze(100, 100, 10, 10, 25, 25, win)
 
     win.wait_for_close()
 
---
+++ maze.py
@@ -0,0 +1,47 @@
+from cell import Cell
+import time
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+        self.__cells = []
+        self.__create_cells()
+
+    def __create_cells(self):
+        for i in range(self.__num_cols):
+            self.__cells.append([])
+
+            for j in range(self.__num_rows):
+                cell = Cell(self.__win)
+                self.__cells[i].append(cell)
+                self.__draw_cell(i, j)
+
+    def __draw_cell(self, i, j):
+        x1 = self.__x1 + (i * self.__cell_size_x)
+        y1 = self.__y1 + (j * self.__cell_size_y)
+        x2 = x1 + self.__cell_size_x
+        y2 = y1 + self.__cell_size_y
+
+        cell = self.__cells[i][j]
+        cell.draw(x1, y1, x2, y2)
+        self.animate()
+
+    def animate(self):
+        self.__win.redraw()
+        time.sleep(0.05)
+
